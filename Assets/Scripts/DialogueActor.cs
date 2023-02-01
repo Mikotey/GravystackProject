552c8ea2aacdec05a60bcc95e6746cbe667e8c26
mikotey <mikotey@gmail.com>
2023-02-01 13:34:12 -0800

    blocking out initial classes

--- Assets/Scripts/DialogueActor.cs
+++ Assets/Scripts/DialogueActor.cs
@@ -2,15 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//instantiate canvas for dialogue moment
-
-//for scriptable objects, run through in order
-
-//screen tap to proceed when at a stopping point
-
-//when no more objects in queue, close dialogue moment and cleanup
-
-public class DialogueMoment : MonoBehaviour
+public class DialogueActor : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
